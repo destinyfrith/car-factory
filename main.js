47cbdaa4ece6d4f5d6218d56a72a9f119b128c76
Destiny Frith <desilyvahn@gmail.com>
2022-01-13 14:59:51 -0600

    car factory exercise complete

--- main.js
+++ main.js
@@ -5,4 +5,40 @@ const createChassis = () => {
     return newChassisObject
 }
 
-const carStart = createChassis()
+// Invoke the function here
+const newCar = createChassis()
+
+// Create a new function 
+const addWheels = (chassisObject) => {
+    chassisObject.wheels = 4
+
+    return chassisObject
+}
+
+const carWithWheels = addWheels(newCar)
+
+const addEngine = (chassisObject) => {
+    chassisObject.engine = "4.8L"
+
+    return chassisObject
+}
+
+const carWithEngine = addEngine(carWithWheels)
+
+const addSteeringWheel = (chassisObject) => {
+    chassisObject.steeringWheel = "Tilting"
+
+    return chassisObject
+}
+
+const carSteeringWheel = addSteeringWheel(carWithEngine)
+
+const addDriveTrain = (chassisObject) => {
+    chassisObject.driveTrain = "two wheel drive"
+
+    return chassisObject
+}
+
+const carDriveTrain = addDriveTrain(carSteeringWheel)
+
+console.log(carDriveTrain)
